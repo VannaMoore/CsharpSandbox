b1b77f9d5bbd1b294a576299f8ef9d1f87e9e9a7
VannaMoore <vannamoore@gmail.com>
2026-01-26 10:41:54 -0600

    Changed 'keep going' feature from recursive function call to do-while loop.

--- ConsoleSkillLab/Exercises/ReverseListPrint.cs
+++ ConsoleSkillLab/Exercises/ReverseListPrint.cs
@@ -1,9 +1,5 @@
 ﻿using System;
 using ConsoleSkillLab.Utilities;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 // Future exercise: Make it to where the user chooses the array size from the start.
 
@@ -16,47 +12,42 @@ namespace ConsoleSkillLab.Exercises
         // Constraint: Don't modify the array
         public static void Reverse()
         {
+            bool keepGoing = false;
             int[] numbersList = new int[5];
-            int[] reverseNumbersList = new int[5];
             int current = 0;
 
-            Console.WriteLine("You will be prompted to enter 5 numbers that will be put into an array.");
-            Console.WriteLine("The program will print them back in the reverse order in which they were entered.\n");
-
-            for (int i = 0; i < numbersList.Length; i++)
+            do
             {
-                Console.Write("Enter a number: ");
-                string? userInput = Console.ReadLine();
+                Console.WriteLine("You will be prompted to enter 5 numbers that will be put into an array.");
+                Console.WriteLine("The program will print them back in the reverse order in which they were entered.\n");
 
-                if (int.TryParse(userInput, out int userNumber))
+                for (int i = 0; i < numbersList.Length; i++)
                 {
-                    numbersList[i] = userNumber;
+                    Console.Write("Enter a number: ");
+                    string? userInput = Console.ReadLine();
+
+                    if (int.TryParse(userInput, out int userNumber))
+                    {
+                        numbersList[i] = userNumber;
+                    }
+                    else
+                    {
+                        Console.WriteLine("That is not valid input. Enter a number.");
+                        i--;
+                    }
                 }
-                else
+
+                Console.WriteLine("\nReverse Numbers List:");
+                for (int i = numbersList.Length - 1; i >= 0; i--)
                 {
-                    Console.WriteLine("That is not valid input. Enter a number.");
-                    i--;
+                    Console.WriteLine($"Index {current}: {numbersList[i]}");
+                    current++;
                 }
+                Console.WriteLine();
+                keepGoing = Helpers.Continue();
             }
-
-            Console.WriteLine("\nReverse Numbers List:");
-            for(int i = numbersList.Length - 1; i >= 0; i--)
-            {
-                reverseNumbersList[current] = numbersList[i];
-                Console.WriteLine($"Index {current}: {numbersList[i]}");
-                current++;
-            }
-            Console.WriteLine();
-
-            bool keepGoing = Helpers.Continue();
-            if (keepGoing)
-            {
-                Reverse();
-            }
-            else
-            {
-                Menu.MainMenu();
-            }
+            while (keepGoing);
+            Menu.MainMenu();
         }
     }
 }
