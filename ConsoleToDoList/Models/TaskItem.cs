20151f11b2e8f09bc81a27256ced1800a67512e6
VannaMoore <vannamoore@gmail.com>
2026-01-30 10:19:22 -0600

    Added new folder ConsoleToDoList
    - Added models and services folder.
    - Added models for TaskItem and TaskList

---
+++ ConsoleToDoList/Models/TaskItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleToDoList.Models
+{
+    internal class TaskItem
+    {
+        private string taskName { get; set; }
+        private string? taskDescription { get; set; }
+        public bool isComplete { get; set; }
+    }
+}
---
+++ ConsoleToDoList/Models/TaskList.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleToDoList.Models
+{
+    internal class TaskList
+    {
+        private string name { get; set; }
+        private List<TaskItem> taskList { get; set; }
+        public DateOnly creationDate { get; set; }
+    }
+}
